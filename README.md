e23cc75bf33a6abd72b64151e47b0913ebb44aea
MattLugar <matt@fieldsclan.com>
2016-05-19 19:41:51 -0400

    Add files via upload
    Initial Code Upload

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
-# wuforecast
-Weather Underground Forecast module for Magic Mirror 2
+# Module: Weather Forecast
+The `weatherforecast` module is one of the default modules of the MagicMirror.
+This module displays the weather forecast for the coming week, including an an icon to display the current conditions, the minimum temperature and the maximum temperature.
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+	{
+		module: 'wuforecast',
+		position: 'top_right',	// This can be any of the regions.
+									// Best results in left or right regions.
+		config: {
+			// See 'Configuration options' for more information.
+			location: 'France/Paris',
+			appid: 'abcde12345abcde12345abcde12345ab' //wunderground.com API key.
+		}
+	}
+]
+````
+
+## Configuration options
+
+The following properties can be configured:
+
+
+<table width="100%">
+	<!-- why, markdown... -->
+	<thead>
+		<tr>
+			<th>Option</th>
+			<th width="100%">Description</th>
+		</tr>
+	<thead>
+	<tbody>
+
+		<tr>
+			<td><code>location</code></td>
+			<td>The location used for weather information.<br>
+				<br><b>US Example:</b> <code>NY/New_York</code>
+				<br><b>Example:</b> <code>France/Paris</code><br>
+                                <br> This value is <b>REQUIRED</b>
+			</td>
+		</tr>
+		<tr>
+			<td><code>appid</code></td>
+			<td>The <a href="https://www.wunderground.com/weather/api/d/pricing" target="_blank">Weather Underground</a> API key, which can be obtained by creating an OpenWeatherMap account. You need either Cumulus or Anvil plan for this module. As long as you make less than 500 queries a day, this is free.<br>
+				<br> This value is <b>REQUIRED</b>
+			</td>
+		</tr>
+		<tr>
+			<td><code>units</code></td>
+			<td>What units to use. Specified by config.js<br>
+				<br><b>Possible values:</b> <code>config.units</code> = Specified by config.js, <code>default</code> = Kelvin, <code>metric</code> = Celsius, <code>imperial</code> =Fahrenheit
+				<br><b>Default value:</b> <code>config.units</code>
+			</td>
+		</tr>
+		<tr>
+			<td><code>maxNumberOfDays</code></td>
+			<td>How many days of forecast to return. Specified by config.js<br>
+				<br><b>Possible values:</b> <code>1</code> - <code>10</code>
+				<br><b>Default value:</b> <code>7</code> (7 days)
+				<br>This value is optional. By default the wuforecast module will return 7 days.
+			</td>
+		</tr>
+		<tr>
+			<td><code>updateInterval</code></td>
+			<td>How often does the content needs to be fetched? (Milliseconds)
+				<br>Note that wunderground updates every 15 minutes maximum. Also free version of API only allows 500 calls per day.
+				<br><b>Possible values:</b> <code>1000</code> - <code>86400000</code>
+				<br><b>Default value:</b> <code>900000</code> (15 minutes)
+			</td>
+		</tr>
+		<tr>
+			<td><code>animationSpeed</code></td>
+			<td>Speed of the update animation. (Milliseconds)<br>
+				<br><b>Possible values:</b><code>0</code> - <code>5000</code>
+				<br><b>Default value:</b> <code>2000</code> (2 seconds)
+			</td>
+		</tr>
+
+		<tr>
+			<td><code>lang</code></td>
+			<td>The language of the days.<br>
+				<br><b>Possible values:</b> <code>en</code>, <code>nl</code>, <code>ru</code>, etc ...
+				<br><b>Default value:</b> uses value of <i>config.language</i>
+			</td>
+		</tr>
+		<tr>
+			<td><code>fade</code></td>
+			<td>Fade the future events to black. (Gradient)<br>
+				<br><b>Possible values:</b> <code>true</code> or <code>false</code>
+				<br><b>Default value:</b> <code>true</code>
+			</td>
+		</tr>
+		<tr>
+			<td><code>fadePoint</code></td>
+			<td>Where to start fade?<br>
+				<br><b>Possible values:</b> <code>0</code> (top of the list) - <code>1</code> (bottom of list)
+				<br><b>Default value:</b> <code>0.25</code>
+			</td>
+		</tr>
+		<tr>
+			<td><code>initialLoadDelay</code></td>
+			<td>The initial delay before loading. If you have multiple modules that use the same API key, you might want to delay one of the requests. (Milliseconds)<br>
+				<br><b>Possible values:</b> <code>1000</code> - <code>5000</code>
+				<br><b>Default value:</b>  <code>0</code>
+			</td>
+		</tr>
+		<tr>
+			<td><code>retryDelay</code></td>
+			<td>The delay before retrying after a request failure. (Milliseconds)<br>
+				<br><b>Possible values:</b> <code>1000</code> - <code>60000</code>
+				<br><b>Default value:</b>  <code>2500</code>
+			</td>
+		</tr>
+		<tr>
+			<td><code>apiBase</code></td>
+			<td>The Weather Underground base URL.<br>
+				<br><b>Default value:</b>  <code>'http://api.wunderground.com/api/'</code>
+			</td>
+		</tr>
+		<tr>
+			<td><code>weatherEndpoint</code></td>
+			<td>The Weather Underground API endPoint.<br>
+				<br><b>Default value:</b>  <code>'/forecast10day/q/'</code>
+			</td>
+		</tr>
+
+	</tbody>
+</table>
---
+++ wuforecast.js
@@ -0,0 +1,256 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: WUForecast
+ *
+ * By Matthew Fields
+ *
+ * Modified from WeatherForecast by Michael Teeuw 
+ * MIT Licensed.
+ */
+
+Module.register("wuforecast",{
+
+	// Default module config.
+	defaults: {
+		location: "",
+		appid: "",
+		units: config.units,
+		maxNumberOfDays: 7,
+		updateInterval: 15 * 60 * 1000, // every 15 minutes
+		animationSpeed: 1000,
+		timeFormat: config.timeFormat,
+		lang: config.language,
+		fade: true,
+		pop: true,
+		fadePoint: 0.25, // Start on 1/4th of the list.
+
+		initialLoadDelay: 2500, // 2.5 seconds delay. This delay is used to keep the wunderground API happy.
+		retryDelay: 2500,
+
+		apiBase: "http://api.wunderground.com/api/",
+		forecastEndpoint: "/forecast10day/q/",
+
+	},
+
+	// Define required scripts.
+	getScripts: function() {
+		return ["moment.js"];
+	},
+
+	// Define required scripts.
+	getStyles: function() {
+		return ["wuforecast.css"];
+	},
+
+	// Define required translations.
+	getTranslations: function() {
+		// The translations for the defaut modules are defined in the core translation files.
+		// Therefor we can just return false. Otherwise we should have returned a dictionary.
+		// If you're trying to build your own module including translations, check out the documentation.
+		return false;
+	},
+
+	// Define start sequence.
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		// Set locale.
+		moment.locale(this.config.lang);
+
+		this.forecast = [];
+		this.loaded = false;
+		this.scheduleUpdate(this.config.initialLoadDelay);
+		this.iconText = null;
+
+		this.updateTimer = null;
+
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+
+		if (this.config.appid === "") {
+			wrapper.innerHTML = "Please set the correct wunderground <i>appid</i> in the config for module: " + this.name + ".";
+			wrapper.className = "dimmed light small";
+			return wrapper;
+		}
+
+		if (this.config.location === "") {
+			wrapper.innerHTML = "Please set the wunderground <i>location</i> in the config for module: " + this.name + ".";
+			wrapper.className = "dimmed light small";
+			return wrapper;
+		}
+
+		if (!this.loaded) {
+			wrapper.innerHTML = this.translate('LOADING');
+			wrapper.className = "dimmed light small";
+			return wrapper;
+		}
+
+		var table = document.createElement("table");
+		table.className = "small";
+
+		for (var f in this.forecast) {
+			var forecast = this.forecast[f];
+
+			var row = document.createElement("tr");
+			table.classname = "row";
+			table.appendChild(row);
+
+			var dayCell = document.createElement("td");
+			dayCell.className = "day";
+			dayCell.innerHTML = forecast.day;
+			row.appendChild(dayCell);
+
+			var iconCell = document.createElement("td");
+			iconCell.className = "bright weather-icon";
+			row.appendChild(iconCell);
+
+			var icon = document.createElement("span");
+			icon.innerHTML = "<img src=\"" + forecast.icon + "\" width=\"35%\" height=\"48%\">";
+			if (forecast.pop > 0 && this.config.pop) {
+				icon.innerHTML = "<sup>" + forecast.pop + "%</sup>" + icon.innerHTML;
+			}
+			console.log("icon: " + this.iconText);
+			iconCell.appendChild(icon);
+
+			var maxTempCell = document.createElement("td");
+			if (this.config.units === "imperial") {
+				maxTempCell.innerHTML = forecast.maxTemp;
+			} else if (this.config.units === "metric") {
+				maxTempCell.innerHTML = forecast.maxTempC;
+			} else {
+				maxTempCell.innerHTML = forecast.maxTempC + 273;
+			}
+			maxTempCell.className = "align-right bright max-temp";
+			row.appendChild(maxTempCell);
+
+			var minTempCell = document.createElement("td");
+                        if (this.config.units === "imperial") {
+                                minTempCell.innerHTML = forecast.minTemp;
+                        } else if (this.config.units === "metric") {
+                                minTempCell.innerHTML = forecast.minTempC;
+                        } else {
+                                minTempCell.innerHTML = forecast.minTempC + 273;
+                        }
+			minTempCell.className = "align-right min-temp";
+			row.appendChild(minTempCell);
+
+			if (this.config.fade && this.config.fadePoint < 1) {
+				if (this.config.fadePoint < 0) {
+					this.config.fadePoint = 0;
+				}
+				var startingPoint = this.forecast.length * this.config.fadePoint;
+				var steps = this.forecast.length - startingPoint;
+				if (f >= startingPoint) {
+					var currentStep = f - startingPoint;
+					row.style.opacity = 1 - (1 / steps * currentStep);
+				}
+			}
+
+		}
+
+		return table;
+	},
+
+	/* updateWeather(compliments)
+	 * Requests new data from wunderground.com.
+	 * Calls processWeather on succesfull response.
+	 */
+	updateWeather: function() {
+		var url = this.config.apiBase + this.config.appid + this.config.forecastEndpoint + this.config.location + ".json";
+		var self = this;
+
+		var retry = true;
+
+		var weatherRequest = new XMLHttpRequest();
+		weatherRequest.open("GET", url, true);
+		weatherRequest.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					try {
+						var parsed = JSON.parse(this.response);
+					}catch(e){
+						console.log("weatherforecast - JSON error: " + e.name);
+						self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
+						return;
+						// here to prevent freezin of app on unfinished JSON.
+					}
+					self.processWeather(parsed);
+				} else if (this.status === 401) {
+					self.config.appid = "";
+					self.updateDom(self.config.animationSpeed);
+
+					Log.error(self.name + ": Incorrect APPID.");
+					retry = false;
+				} else {
+					Log.error(self.name + ": Could not load weather.");
+				}
+
+				if (retry) {
+					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
+				}
+			}
+		};
+		weatherRequest.send();
+	},
+
+	/* processWeather(data)
+	 * Uses the received data to set the various values.
+	 *
+	 * argument data object - Weather information received form wunderground.
+	 */
+	processWeather: function(data) {
+
+		this.forecast = [];
+		for (var i = 0, count = Math.min(data.forecast.simpleforecast.forecastday.length, this.config.maxNumberOfDays); i < count; i++) {
+			
+			var forecast = data.forecast.simpleforecast.forecastday[i];
+			this.forecast.push({
+
+				day: moment(forecast.date.epoch, "X").format("ddd"),
+				icon: forecast.icon_url,
+				pop: forecast.pop,
+				maxTemp: this.roundValue(forecast.high.fahrenheit),
+				minTemp: this.roundValue(forecast.low.fahrenheit),
+				maxTempC: this.roundValue(forecast.high.celsius),
+				minTempC: this.roundValue(forecast.low.celsius)
+
+			});
+		}
+
+		this.loaded = true;
+		this.updateDom(this.config.animationSpeed);
+	},
+
+	/* scheduleUpdate()
+	 * Schedule next update.
+	 *
+	 * argument delay number - Milliseconds before next update. If empty, this.config.updateInterval is used.
+	 */
+	scheduleUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		var self = this;
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(function() {
+			self.updateWeather();
+		}, nextLoad);
+	},
+
+	/* function(temperature)
+	 * Rounds a temperature a whole number.
+	 *
+	 * argument temperature number - Temperature.
+	 *
+	 * return number - Rounded Temperature.
+	 */
+	roundValue: function(temperature) {
+		return Math.round(temperature);
+	}
+});
